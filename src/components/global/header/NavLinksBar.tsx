c96928ab8ff856c6189b24c6b3ffc44f31cb15c0
Anik Ahmed <anikahmed0091@gmail.com>
2023-01-27 00:26:42 +0600

    ✨ feat: add dark button

--- src/components/global/header/NavLinksBar.tsx
+++ src/components/global/header/NavLinksBar.tsx
@@ -6,40 +6,40 @@ interface Props {
 
 // TODO: should use localStorage?
 
-const NavLinkBar = (props: Props) => {
+const NavLinksBar = (props: Props) => {
   return (
     <>
-      <div className="sticky top-2 pt-4 shadow-md font-josefin font-bold px-10 rounded-md mx-10 md:mx-96 bg-white">
-        <div className="flex justify-around">
+      <div className="sticky top-2 flex justify-center">
+        <div className="inline w-fit pt-4 pb-2 shadow-md font-josefin font-bold px-4 md:px-8 rounded-md bg-white">
           <div
             className={`${
-              props.title == "Gallery" ? "border-b-2" : ""
-            } inline hover:border-b-2 border-gray-300 pb-1 px-2`}
+              props.title == "Gallery" ? "border-b-4" : ""
+            } inline hover:border-b-4 border-gray-300 pb-2 px-2 md:px-4 mx-2 md:mx-4`}
           >
             <a href="/">Gallery</a>
           </div>
           <div
             className={`${
-              props.title == "About" ? "border-b-2" : ""
-            } inline hover:border-b-2 border-gray-300 pb-1 px-2`}
+              props.title == "About" ? "border-b-4" : ""
+            } inline hover:border-b-4 border-gray-300 pb-2 px-2 md:px-4 mx-2 md:mx-4`}
           >
             <a href="/about">About</a>
           </div>
           <div
             className={`${
-              props.title == "Contact" ? "border-b-2" : ""
-            } inline hover:border-b-2 border-gray-300 pb-1 px-2`}
+              props.title == "Contact" ? "border-b-4" : ""
+            } inline hover:border-b-4 border-gray-300 pb-2 px-2 md:px-4 mx-2 md:mx-4`}
           >
             <a href="/contact">Contact</a>
           </div>
+          {/* // TODO: fix position */}
+          <div className="inline pb-2 ">
+            <span>🌙</span>
+          </div>
         </div>
-        {/* // TODO: fix position */}
-        {/* <div className="inline border-gray-300 pb-1 w-1/5">
-          <span>🌙</span>
-        </div> */}
       </div>
     </>
   );
 };
 
-export default NavLinkBar;
+export default NavLinksBar;
