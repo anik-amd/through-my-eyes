9eb8b3aab277954ef445740fd203b148988a5a91
Anik Ahmed <anikahmed0091@gmail.com>
2023-01-26 14:30:32 +0600

    ✨ feat: update NavLinksBar

--- src/components/global/header/NavLink.tsx
+++ src/components/global/header/NavLink.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 export interface Props {
   href: string;
@@ -6,22 +6,7 @@ export interface Props {
 }
 
 const NavLink = (props: Props) => {
-  const [isActive, setIsActive] = useState(false);
-
-  // const handleClick = (event: React.MouseEvent) => {
-  //   setIsActive((current) => !current);
-  //   // event.preventDefault();
-  // };
-
-  return (
-    <a
-      // className={`hover:border-b-2 border-gray-300`}
-      // onClick={handleClick}
-      href={props.href}
-    >
-      {props.children}
-    </a>
-  );
+  return <a href={props.href}>{props.children}</a>;
 };
 
 export default NavLink;
--- src/components/global/header/NavLinksBar.tsx
+++ src/components/global/header/NavLinksBar.tsx
@@ -4,15 +4,17 @@ interface Props {
   title?: string;
 }
 
+// todo: should use localStorage?
+
 const NavLinkBar = (props: Props) => {
   return (
     <div className="font-josefin font-bold flex justify-around mx-24 md:mx-96">
       <div
         className={`${
-          props.title == "Home" ? "border-b-2" : ""
+          props.title == "Gallery" ? "border-b-2" : ""
         } inline hover:border-b-2 border-gray-300 pb-1 px-2`}
       >
-        <a href="/">Home</a>
+        <a href="/">Gallery</a>
       </div>
       <div
         className={`${
