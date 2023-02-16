134f43c675f6df44d5c93ab7b271b9c24d476541
Anik Ahmed <anikahmed0091@gmail.com>
2023-02-16 23:41:52 +0600

    ✨ feat: add links to image

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,18 +1,21 @@
 import PhotoAlbum from "react-photo-album";
 
-import photos from "./photos";
-
 import { formattedGalleryCollection } from "../utils/formattedGallery";
 
 const Gallery = () => {
-  console.log(photos);
   console.log(formattedGalleryCollection);
+  // TODO: add custom photo render.
   return (
-    <div className="mb-5">
+    <div className="mb-5 cursor-pointer">
       <PhotoAlbum
         targetRowHeight={300}
         layout="rows"
         photos={formattedGalleryCollection}
+        onClick={(o) => {
+          console.log(o.photo.slug);
+          const slug = "/" + o.photo.slug;
+          location.href = slug;
+        }}
       />
     </div>
   );
