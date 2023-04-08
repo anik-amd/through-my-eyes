b8a348de6499cb7cc382b1622eba9b542f40a2c1
Anik Ahmed <anikahmed0091@gmail.com>
2023-04-08 16:36:31 +0600

    ✨ feat: add dark theme

--- src/components/SingleImage.tsx
+++ src/components/SingleImage.tsx
@@ -15,15 +15,17 @@ const SingleImage = (props: Props) => {
       <div className="mb-5">
         <div className="flex flex-col w-full justify-center items-center">
           <img
-            className="mx-auto w-screen md:w-3/4 lg:w-1/2 mb-5"
+            className="mx-auto w-screen md:w-3/4 lg:w-1/2 mb-5 rounded-xl shadow-sm shadow-gray-700 dark:shadow-gray-700"
             src={imageObj?.src}
             alt={imageObj?.title}
           />
           <div className="mb-5 w-screen md:w-3/4 lg:w-1/2">
-            <h1 className="font-josefin text-2xl font-semibold text-gray-800">
+            <h1 className="font-josefin text-2xl font-semibold text-gray-800 dark:text-gray-400">
               {imageObj?.title}
             </h1>
-            <p className="text-base text-gray-900">{imageObj?.content}</p>
+            <p className="text-base text-gray-900 dark:text-gray-300">
+              {imageObj?.content}
+            </p>
           </div>
         </div>
       </div>
--- src/components/global/header/NavLinksBar.tsx
+++ src/components/global/header/NavLinksBar.tsx
@@ -23,25 +23,25 @@ const NavLinksBar = (props: Props) => {
   return (
     <>
       <div className="sticky top-2 flex justify-center">
-        <div className="inline w-fit pt-4 pb-2 shadow-md font-josefin font-bold px-4 md:px-8 rounded-md bg-white">
+        <div className="inline w-fit pt-4 pb-2 shadow-md font-josefin font-bold px-4 md:px-8 rounded-md bg-white dark:bg-gray-700 dark:text-gray-300">
           <div
             className={`${
               props.title == "Gallery" ? "border-b-4" : ""
-            } inline hover:border-b-4 border-gray-300 pb-2 px-2 md:px-4 mx-2 md:mx-4`}
+            } inline hover:border-b-4 border-gray-300 dark:border-gray-600 pb-2 px-2 md:px-4 mx-2 md:mx-4`}
           >
             <a href="/">Gallery</a>
           </div>
           <div
             className={`${
               props.title == "About" ? "border-b-4" : ""
-            } inline hover:border-b-4 border-gray-300 pb-2 px-2 md:px-4 mx-2 md:mx-4`}
+            } inline hover:border-b-4 border-gray-300 dark:border-gray-600 pb-2 px-2 md:px-4 mx-2 md:mx-4`}
           >
             <a href="/about">About</a>
           </div>
           <div
             className={`${
               props.title == "Contact" ? "border-b-4" : ""
-            } inline hover:border-b-4 border-gray-300 pb-2 px-2 md:px-4 mx-2 md:mx-4`}
+            } inline hover:border-b-4 border-gray-300 dark:border-gray-600 pb-2 px-2 md:px-4 mx-2 md:mx-4`}
           >
             <a href="/contact">Contact</a>
           </div>
